048aba34aaceb9bae3ac4d58fa7f5591f0a240bf
paulotijero <paulotijero@gmail.com>
2022-01-19 12:24:36 -0500

    ADD: blank pages to new routes

---
+++ app/javascript/views/sms/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function SMS(){
+  return (
+    <span>This is a SMS</span>
+  )
+}
---
+++ app/javascript/views/superadmin/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Superadmin(){
+  return (
+    <span>This is a Superadmin</span>
+  )
+}
