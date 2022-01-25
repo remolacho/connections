d87f015f8176b30cb2298e36f0b3ceca91cf87d0
paulotijero <paulotijero@gmail.com>
2022-01-25 17:58:47 -0500

    ADD: login page

---
+++ app/javascript/views/auth/login.js
@@ -0,0 +1,82 @@
+import React from "react";
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Row,
+  Col,
+} from "reactstrap";
+
+export default function Login(){
+  return (
+    <>
+      <Col lg="5" md="7">
+        <Card className="bg-secondary cn-shadow border-0">
+        <CardBody className="px-lg-5 py-lg-5">
+            <Form role="form">
+              <FormGroup className="mb-3">
+                <InputGroup className="input-group-alternative">
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="ni ni-email-83" />
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    placeholder="Email"
+                    type="email"
+                    autoComplete="new-email"
+                  />
+                </InputGroup>
+              </FormGroup>
+              <FormGroup>
+                <InputGroup className="input-group-alternative">
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="ni ni-lock-circle-open" />
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    placeholder="Password"
+                    type="password"
+                    autoComplete="new-password"
+                  />
+                </InputGroup>
+              </FormGroup>
+              <div className="custom-control custom-control-alternative custom-checkbox">
+                <input
+                  className="custom-control-input"
+                  id=" customCheckLogin"
+                  type="checkbox"
+                />
+                <label
+                  className="custom-control-label"
+                  htmlFor=" customCheckLogin"
+                >
+                  <span className="text-muted">Remember me</span>
+                </label>
+              </div>
+              <div className="text-center">
+                <Button className="my-4" color="info" type="button" block>
+                  Ingresar
+                </Button>
+                <Button className="my-4" color="info" outline type="button" block>
+                  Crear cuenta
+                </Button>
+                <Button className="my-4" color="info" outline type="button" block>
+                  Recuperar clave
+                </Button>
+              </div>
+            </Form>
+          </CardBody>
+        </Card>
+      </Col>
+    </>
+  );
+};
