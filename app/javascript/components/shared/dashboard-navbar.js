3eb50f360e842c89d710c363104f162b742d0910
paulotijero <paulotijero@gmail.com>
2022-02-03 01:36:52 -0500

    ADD: dashboard navbar options

---
+++ app/javascript/components/shared/dashboard-navbar.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { NavLink as NavLinkRRD, Outlet } from  "react-router-dom"
+import { 
+  Container, 
+  Row, 
+  Col,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavLink,
+  NavItem,
+  Collapse
+} from 'reactstrap'
+
+export default function DashboardNavbar(){
+
+  return (
+    <Container className='pt-7' fluid>
+      <Row>
+        <Col>
+          <Navbar
+            expand="md"
+            light
+          >
+            <NavbarBrand 
+              to="/dashboard"
+              tag={NavLinkRRD}
+            >
+              Admin Dashboard
+            </NavbarBrand>
+            <Collapse navbar>
+              <Nav
+                className="me-auto"
+                navbar
+              >
+                <NavItem>
+                  <NavLink 
+                    to="/dashboard/sales"
+                    tag={NavLinkRRD}
+                  >
+                    Ventas
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink 
+                    to="/dashboard/messages"
+                    tag={NavLinkRRD}
+                  >
+                    Mensajes
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </Col>
+      </Row>
+
+      {/* content dashboard */}
+      <Outlet />
+    </Container>
+  )
+
+}
