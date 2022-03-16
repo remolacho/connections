599ed848f099c4ac46d2be21a0c7318e7072c6a5
paulotijero <paulotijero@gmail.com>
2022-03-15 20:28:17 -0500

    ADD: modal inside contact page

--- app/javascript/views/contact/index.js
+++ app/javascript/views/contact/index.js
@@ -11,15 +11,18 @@ import {
   Row,
   Col,
   Button,
-  Input
+  Input,
+  Modal
 } from "reactstrap";
 
 export default function Index(){
+  const [showModalListContact, setShowModalListContact] = React.useState(false)
+
   return (
     <Container className="pt-7" fluid>
         <div className="d-flex justify-content-between">
           <h1 className="mb-0">Contactos</h1>
-          <Button className="btn-outline-danger">
+          <Button onClick={() => setShowModalListContact(!showModalListContact)} className="btn-outline-danger">
             Agregar contacto
           </Button>
         </div>
@@ -218,6 +221,138 @@ export default function Index(){
           </Card>
         </div>
       </Row>
+
+      {/*  Modal List Contact - START  */}
+      <Modal
+        className="modal-dialog-centered"
+        size="lg"
+        isOpen={showModalListContact}
+        toggle={() => setShowModalListContact(!showModalListContact)}
+      >
+        <div className="modal-header">
+          <h6 className="modal-title text-lg" id="modal-title-default">
+            Lista de contactos
+          </h6>
+          <button
+            aria-label="Close"
+            className="close"
+            data-dismiss="modal"
+            type="button"
+            onClick={() => setShowModalListContact(!showModalListContact)}
+          >
+            <span aria-hidden={true}>×</span>
+          </button>
+        </div>
+        <div className="modal-body">
+          <p className="align-items-center font-weight-600 text-sm d-flex">
+            <span 
+              className="align-items-center bg-info text-sm text-white d-flex justify-content-center mr-2 rounded-circle" 
+              style={{height: '24px', width: '24px'}}>
+              1
+            </span>
+            Crear Lista
+          </p>
+          <div className="alert fade show" role="alert" style={{ backgroundColor: "#C1D5FC", borderColor: "#C1D5FC" }}>
+            <span className="d-inline-block">
+              El primer encabezado de la <strong>primera columna</strong> deber decir <strong>"telefono" o "email"</strong>
+            </span>
+            <button
+              aria-label="Close Alert"
+              className="close"
+              type="button"
+            >
+              <span className="text-white" aria-hidden={true}>×</span>
+            </button>
+          </div>
+          <div className="alert fade show" role="alert" style={{ backgroundColor: "#C1D5FC", borderColor: "#C1D5FC" }}>
+            <span className="d-inline-block">
+              <strong>CSV</strong> El archivo en formato
+            </span>
+            <button
+              aria-label="Close Alert"
+              className="close"
+              type="button"
+            >
+              <span className="text-white" aria-hidden={true}>×</span>
+            </button>
+          </div>
+          <div className="my-4 text-center">
+            <img
+              alt="Ejemplo CSV"
+              className=""
+              src={require("assets/img/spreadsheet.png")}
+            />
+            <p className="my-2">Ejemplo CSV</p>
+            <Button 
+              color="warning" 
+              type="button"
+              className="my-2"
+            >
+              Descargar CSV Ejemplo
+            </Button>
+          </div>
+          <p className="align-items-center font-weight-600 text-sm d-flex">
+            <span 
+              className="align-items-center bg-info text-sm text-white d-flex justify-content-center mr-2 rounded-circle" 
+              style={{height: '24px', width: '24px'}}>
+              2
+            </span>
+            Cargar Archivo
+          </p>
+          <form>
+            <div className="form-group mb-2">
+              <label htmlFor="name-list" className="font-weight-600 text-sm">Mi lista de contactos</label>
+              <input type="email" className="form-control" id="name-list" placeholder="Mi lista de contactos" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="file-list" className="font-weight-600 text-sm">Lista</label>
+              <input type="file" className="form-control-file" id="file-list" />
+            </div>
+          </form>
+          <div className="text-center">
+            <svg className="mx-2 mb-1 fill-current">
+              <use xlinkHref="#error" />
+            </svg>
+            <p className="text-xl mb-1 font-weight-600">La lista no se cargó</p>
+            <p>La lista ingresada tiene los siguientes errores...</p>
+          </div>
+          <div className="alert fade show" role="alert" style={{ backgroundColor: "#C1D5FC", borderColor: "#C1D5FC" }}>
+            <span className="d-inline-block">
+              La fila 43 presenta caracteres que <strong>no cumplen UTF-8</strong>
+            </span>
+            <button
+              aria-label="Close Alert"
+              className="close"
+              type="button"
+            >
+              <span className="text-white" aria-hidden={true}>×</span>
+            </button>
+          </div>
+          <div className="text-center">
+            <svg className="mx-2 mb-1 fill-current">
+              <use xlinkHref="#confirmation" />
+            </svg>
+            <p className="text-xl mb-1 font-weight-600">La lista fue cargada</p>
+            <p>La lista ingresada fue cargada con éxito!</p>
+          </div>
+        </div>
+        <div className="modal-footer">
+          <Button color="info" type="button">
+            Aceptar
+          </Button>
+          <Button
+            className="ml-auto"
+            color="link"
+            data-dismiss="modal"
+            type="button"
+            onClick={() => setShowModalListContact(!showModalListContact)}
+          >
+            Cancelar
+          </Button>
+        </div>
+      </Modal>
+      {/*  Modal List Contact - END  */}
+
     </Container>
   )
 }
