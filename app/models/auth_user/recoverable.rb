daafcc018f628110ad806499df70dbb2edd6a3ae
remolacho <jonathangrh.25@gmail.com>
2022-03-29 12:38:24 -0500

    se arregla bug para recuperar password a personas antiguas del sistema

--- app/models/auth_user/recoverable.rb
+++ app/models/auth_user/recoverable.rb
@@ -6,6 +6,8 @@ class AuthUser
       begin
         self.reset_password_key = SecureRandom.urlsafe_base64
       end while AuthUser.exists?(reset_password_key: reset_password_key)
+
+      self.password = BCrypt::Password.create('P4ssW02d!') unless is_migrated
       self.reset_password_key_expires_at = expired
       save!
     end
